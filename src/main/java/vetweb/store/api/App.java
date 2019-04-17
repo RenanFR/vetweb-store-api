75680bc1db6605bc963b19b3a65871d5be2bb6c6
Renan Rodrigues <renanfr1047@gmail.com>
2019-04-16 22:42:24 -0300

    First commit

---
+++ src/main/java/vetweb/store/api/App.java
@@ -0,0 +1,13 @@
+package vetweb.store.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
