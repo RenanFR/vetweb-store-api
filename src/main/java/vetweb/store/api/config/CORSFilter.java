671f3980eeb82a9bd6e6359afe6db7b9ae8b500f
Renan Rodrigues <renanfr1047@gmail.com>
2019-04-17 23:45:46 -0300

    Cross domain filter and product save request fix

---
+++ src/main/java/vetweb/store/api/config/CORSFilter.java
@@ -0,0 +1,44 @@
+package vetweb.store.api.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CORSFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, Connection, User-Agent, authorization, sw-useragent, sw-version");
+
+        if (httpServletRequest.getMethod().equals("OPTIONS")) {
+        	httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+        chain.doFilter(request, response);			
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
