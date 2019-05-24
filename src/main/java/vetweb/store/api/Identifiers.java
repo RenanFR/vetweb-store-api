b25c1d9e3f71ce46e029c329e0a408fe33746cd9
renan.rodrigues <renan.rodrigues@accountfy.com>
2019-05-24 17:03:27 -0300

    Identifiers list

---
+++ src/main/java/vetweb/store/api/Identifiers.java
@@ -0,0 +1,14 @@
+package vetweb.store.api;
+
+public class Identifiers {
+	
+	//adjustPriceRange
+	//price
+	//value
+	//productPrice
+	//rangeInput
+	//currentPriceRange
+	//Handling price range
+	//Identifiers list
+	
+}
