f0d0295b7e3f5c1c3abd10a98d7da1194a1fc8ec
Renan Rodrigues <renanfr1047@gmail.com>
2019-05-26 10:36:13 -0300

    Handling session end

--- src/main/java/vetweb/store/api/config/security/jwt/JWTAuthenticationVerifier.java
+++ src/main/java/vetweb/store/api/config/security/jwt/JWTAuthenticationVerifier.java
@@ -7,10 +7,13 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
+
+import io.jsonwebtoken.ExpiredJwtException;
 
 //Verifies the token existence in a common request to authorize response
 public class JWTAuthenticationVerifier extends GenericFilterBean{
@@ -18,10 +21,16 @@ public class JWTAuthenticationVerifier extends GenericFilterBean{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		Authentication authentication = TokenService
-				.getTokenFromRequest((HttpServletRequest) request);
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		chain.doFilter(request, response);
+		try {
+			Authentication authentication = TokenService
+					.getTokenFromRequest((HttpServletRequest) request);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			chain.doFilter(request, response);
+		} catch (ExpiredJwtException jwtException) {
+			HttpServletResponse httpServletResponse = (HttpServletResponse)response;
+			httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Token provided is expired");
+			return;
+		}
 		
 	}
 
