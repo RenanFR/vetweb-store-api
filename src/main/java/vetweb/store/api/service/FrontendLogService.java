9d8dd0bb4cc992bc6d2a07ce6585e973cdfbf6a7
renan.rodrigues <renan.rodrigues@accountfy.com>
2019-05-28 09:17:34 -0300

    More information about log

--- src/main/java/vetweb/store/api/service/FrontendLogService.java
+++ src/main/java/vetweb/store/api/service/FrontendLogService.java
@@ -1,8 +1,12 @@
 package vetweb.store.api.service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import vetweb.store.api.models.Log;
 import vetweb.store.api.persistence.LogRepository;
@@ -13,8 +17,10 @@ public class FrontendLogService {
 	@Autowired
 	private LogRepository logRepository;
 	
-	public String saveLog(@RequestBody Log log) {
-		return logRepository.save(log).getLogPrimaryKey().getUser();
+	public String saveLog(Log log) {
+		String user = logRepository.save(log).getLogPrimaryKey().getUser();
+		String logTime = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").format(LocalDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(log.getLogPrimaryKey().getTimeStamp())), TimeZone.getDefault().toZoneId()));
+		return "Logged saved for user " + user + " at " + logTime;
 	}
 
 }
